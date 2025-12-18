0c0e9d699db3ff0424d10f8a135d7314f1eb1fb4
ThanapatrP <thanapatr.pul@gmail.com>
2025-12-18 22:19:29 +0700

    learn useState()

---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+function Button(){
+    const handleClick = (e) => {e.target.textContent = "Ouch!"}
+
+    return (<button onClick={(e) => handleClick(e)}>Click me</button>);
+}
+
+export default Button;
---
+++ src/MyComponent.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'; // import only useState function from React using '{}' to destructuring
+
+function MyComponent(){
+    
+    // use '[]' for array destructuring getting only name variable and setName() function
+    // use useState() for creating variable that trigger rerender the DOM
+    // const [name, setName] = useState();
+    const [name, setName] = useState("Gabe"); // put default value inside useState(<DefVal>)
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
+
+    const updateName = () => {
+        // name = "JokeIScream"; <- this wouldnt work
+        setName("Joke iScream");
+    }
+
+    const IncrementAge = () => {
+        setAge(age + 67);
+    }
+
+    const toggleEmployed = () => {
+        setIsEmployed(!isEmployed);
+    }
+
+    return <div>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Is Employed: {isEmployed ? "YEAH" : "NAH"}</p>
+        <button onClick={updateName}>Set name</button>
+        <button onClick={IncrementAge}>Increase Age</button>
+        <button onClick={toggleEmployed}>Toggle Employed</button>
+    </div>
+
+}
+
+export default MyComponent;
